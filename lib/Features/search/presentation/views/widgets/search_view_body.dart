eafa36435ab06b7898a6bac1fe6476fbb0f1868c
Noha0212 <nonathabit2002@gmail.com>
2024-10-04 03:23:30 +0300

    home repo with either

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/styles.dart';
-import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
+import '../../../../home/presentation/views/widgets/book_list_view_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
