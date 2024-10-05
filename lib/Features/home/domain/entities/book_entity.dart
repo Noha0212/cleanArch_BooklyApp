0b56a3562875a6a0fac9451036a916a2f847bf70
Noha0212 <nonathabit2002@gmail.com>
2024-10-05 04:46:31 +0300

    generate book entity adapter

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,9 +1,19 @@
-class BookEntity {
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
+class BookEntity extends HiveObject {
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String? image;
+  @HiveField(3)
   final String? authName;
+  @HiveField(4)
   final num? price;
+  @HiveField(5)
   final num? rating;
 
   BookEntity(
