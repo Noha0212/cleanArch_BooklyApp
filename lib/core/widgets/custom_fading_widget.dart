20e9227c53e684bbc6acb9256f96bdff5d9e8c30
Noha0212 <nonathabit2002@gmail.com>
2024-10-09 07:58:57 +0300

    create custom fading widget

---
+++ lib/core/widgets/custom_fading_widget.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CustomFadingWidget extends StatefulWidget {
+  const CustomFadingWidget({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+  final Widget child;
+  @override
+  State<CustomFadingWidget> createState() => _CustomFadingWidgetState();
+}
+
+class _CustomFadingWidgetState extends State<CustomFadingWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(vsync: this);
+    animation =
+        Tween<double>(begin: 0.2, end: 0.8).animate(animationController);
+    animationController.addListener(() {
+      setState(() {});
+    });
+    animationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: animation.value,
+      child: widget.child,
+    );
+  }
+}
